130a637d9fc5a8744a90b1f5b6697fa598286ae7
Wambui Sharon <wambuisha50@gmail.com>
2017-05-10 15:54:13 -0400

    Adding more test

--- prime_number_test.py
+++ prime_number_test.py
@@ -8,6 +8,15 @@ class test_prime_numbers(unittest.TestCase):
         self.assertEquals(generate_primes(9),[2,3,5,7])
     def test_for_negative_input(self):
         self.assertEquals(generate_primes(-1), 'Negative numbers are not permited')
+    def test_2(self):
+        self.assertFalse(generate_primes(2), False)
+    def test_1(self):
+        self.assertFalse(generate_primes(1), False)
+    def test_0(self):
+        self.assertFalse(generate_primes(0), False)
+    def test_7(self):
+        self.assertTrue(generate_primes(7),True)
+ 
 
 if __name__== "__main__":
     unittest.main()
